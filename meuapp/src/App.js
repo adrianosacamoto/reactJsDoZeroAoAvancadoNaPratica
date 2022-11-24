f8fd580ed6a84ca0f9a992db81055565fc9debe6
adrianosacamoto <adrianosacamoto@gmail.com>
2022-11-24 19:35:15 -0300

    Rotas

--- meuapp/src/App.js
+++ meuapp/src/App.js
@@ -1,45 +1,9 @@
-import React, { useEffect, useState } from 'react';
-import './style.css';
-// https://sujeitoprogramador.com/rn-api/?api=posts
+import RoutesApp from './routes';
 
 function App() {
-  const [nutri, setNutri] = useState([]);
-
-  useEffect(() => {
-
-    function loadApi() {
-      let url = 'https://sujeitoprogramador.com/rn-api/?api=posts';
-      fetch(url)
-        .then((r) => r.json())
-        .then((jason) => {
-          console.log(jason);
-          setNutri(jason);
-        })
-    }
-
-    loadApi();
-  }, []);
-
   return (
-    <div className='container'>
-      <header>
-        <strong>React Nutri</strong>
-      </header>
-
-      {nutri.map((item) => {
-        return (
-          <article key={item.id} className='post'>
-            <strong className='titulo'>{item.titulo}</strong>
-            <img src={item.capa} alt={item.titulo} className='capa' />
-            <p className='subtitulo'>
-              {item.subtitulo}
-            </p>
-            <a className='botao'>Acessar</a>
-          </article>
-        )
-      })}
-    </div>
-  )
+    <RoutesApp/>
+  );
 }
 
 export default App;
